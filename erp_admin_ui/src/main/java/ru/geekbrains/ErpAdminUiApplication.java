73bd7ccb41ab593cf1edbbf306a59634524997ce
Alexey Smisloff <a.smisloff@gmail.com>
2021-05-15 23:24:06 +0300

    autoConfig

--- erp_admin_ui/src/main/java/ru/geekbrains/ErpAdminUiApplication.java
+++ erp_admin_ui/src/main/java/ru/geekbrains/ErpAdminUiApplication.java
@@ -1,9 +1,13 @@
 package ru.geekbrains;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("ru.geekbrains")
 public class ErpAdminUiApplication {
 
     public static void main(String[] args) {

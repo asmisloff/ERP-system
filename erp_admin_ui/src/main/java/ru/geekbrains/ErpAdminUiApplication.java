aa9ffa708ddb0cbe8fbd5878ede0970723a7e357
Alexey Smisloff <a.smisloff@gmail.com>
2021-05-16 01:48:25 +0300

    Настройки для heroku local

--- erp_admin_ui/src/main/java/ru/geekbrains/ErpAdminUiApplication.java
+++ erp_admin_ui/src/main/java/ru/geekbrains/ErpAdminUiApplication.java
@@ -1,10 +1,18 @@
 package ru.geekbrains;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableAutoConfiguration

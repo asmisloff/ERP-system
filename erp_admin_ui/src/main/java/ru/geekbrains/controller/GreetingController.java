d668c146438ce56ea83cb336c35d56c72b68f9e7
Alexey Smisloff <a.smisloff@gmail.com>
2021-05-15 23:33:34 +0300

    server.port=${PORT:5000}, исправил контроллер

--- erp_admin_ui/src/main/java/ru/geekbrains/controller/GreetingController.java
+++ erp_admin_ui/src/main/java/ru/geekbrains/controller/GreetingController.java
@@ -1,10 +1,12 @@
 package ru.geekbrains.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreetingController {
 
+    @GetMapping("/")
     public String greet() {
         return "greet";
     }

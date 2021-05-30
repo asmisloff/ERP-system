cb94199efdd084bb6103ff22a875bb649d603e78
asmisloff <a.smisloff@gmail.com>
2021-05-30 12:51:32 +0300

    liquibase: bean, plugin, настройки, стартовый набор миграций.

---
+++ erp_database/src/main/resources/db/changelog/01/insert_test_drawings_and_thumbs.sql
@@ -0,0 +1,16 @@
+INSERT INTO drawings VALUES
+    (1, 'drawings/ДМ 311-09.30.00 СБ -- Приводной вал.pdf'),
+    (2, 'drawings/ДМ 311-09.10.16 -- шестерня.pdf'),
+    (3, 'drawings/ДМ 311-09.10.00 СБ -- редуктор.pdf'),
+    (4, 'drawings/ДМ 311-09.10.15 -- Вал.pdf'),
+    (5, 'drawings/ДМ 311-09.20.00 СБ -- муфта.pdf'),
+    (6, 'drawings/ДМ 311-09.00.00 BO -- Привод.pdf');
+
+INSERT INTO drawing_thumbnails VALUES
+    (1, 'drawings/thumbs/ДМ 311-09.30.00 СБ -- Приводной вал_page_0.png', 1),
+    (2, 'drawings/thumbs/ДМ 311-09.10.16 -- шестерня_page_0.png', 2),
+    (3, 'drawings/thumbs/ДМ 311-09.10.00 СБ -- редуктор_page_0.png', 3),
+    (4, 'drawings/thumbs/ДМ 311-09.10.00 СБ -- редуктор_page_1.png', 3),
+    (5, 'drawings/thumbs/ДМ 311-09.10.15 -- Вал_page_0.png', 4),
+    (6, 'drawings/thumbs/ДМ 311-09.20.00 СБ -- муфта_page_0.png', 5),
+    (7, 'drawings/thumbs/ДМ 311-09.00.00 BO -- Привод_page_0.png', 6);

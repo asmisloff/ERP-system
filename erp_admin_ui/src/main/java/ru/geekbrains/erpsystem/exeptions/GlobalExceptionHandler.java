b314d475a5162175c9ebe94a20dd1ac743ce9959
Alexey Smisloff <a.smisloff@gmail.com>
2021-06-01 19:46:06 +0300

    Добавлен ControllerAdvice. Он перехватывает все исключения и перенаправляет на страницу error. На странице error выводится сообщение перехваченного исключения.

---
+++ erp_admin_ui/src/main/java/ru/geekbrains/erpsystem/exeptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.erpsystem.exeptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneralException(Exception e, Model m, HttpServletRequest request) {
+        m.addAttribute("message", e.getMessage());
+        m.addAttribute("referer", request.getHeader("referer"));
+        logger.error(e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceededException(Exception e, Model m, HttpServletRequest request) {
+        m.addAttribute("message", "Файл слишком велик. Нельзя загрузить больше 1 МБ");
+        m.addAttribute("referer", request.getHeader("referer"));
+        logger.error(e.getMessage());
+        return "error";
+    }
+
+}
